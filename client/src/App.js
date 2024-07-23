31242193ea0e24a7dc71e300b44b7bfc83d9f827
Jason <murangira99@gmail.com>
2024-07-23 21:06:49 +0200

    created

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Homepage from './components/Homepage'
-import Login from './components/Login'
-import Register from './components/Register'
-import NoPage from './components/NoPage'
+
 
 
 function App() {
   return (
     <div className="App">
-         <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="login" element={<Login />} />
-        <Route path="home" element={<Homepage />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
-    </BrowserRouter>
+         Home
     </div>
   );
 }
