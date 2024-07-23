245a07a30079c95005a6bbc7ebd3ada29c75d7b9
Jason <murangira99@gmail.com>
2024-07-23 20:59:05 +0200

    created path routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      Hello world
+         <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="home" element={<Homepage />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
