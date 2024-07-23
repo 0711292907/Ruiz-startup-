e5edeb575effbf281708ed037a8336a24427f441
Jason <murangira99@gmail.com>
2024-07-23 21:03:45 +0200

    related

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './components/Homepage'
+import Login from './components/Login'
+import Register from './components/Register'
+import NoPage from './components/NoPage'
+
 
 function App() {
   return (
