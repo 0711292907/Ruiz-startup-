dfb83efd507a84145a480a8afffa5f6f39aa05df
Jason <murangira99@gmail.com>
2024-07-24 20:09:03 +0200

    function App() {   return (     <div className="App">       <BrowserRouter>         <Routes>           <Route path="/" element={<Register />} />           <Route path="/Login" element={<Login />} />           <Route path="/NoPage" element={<NoPage />} />           <Route path="/Homepage" element={<Homepage />} />         </Routes>       </BrowserRouter>     </div>   ); }
    export default App;

--- client/src/App.js
+++ client/src/App.js
@@ -4,5 +4,20 @@ import Login from './components/Login';
 import Register from './components/Register';
 import Nopage from './components/Nopage';
 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Nopage" element={<Noage />} />
+          <Route path="/Homepage" element={<Homepage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
+export default App;
 
