ef0004091b0f2589125533760b297f5db2955629
Jason <murangira99@gmail.com>
2024-07-24 19:58:28 +0200

    removed capitcal letter on no pahe

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
 import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Register from './components/Register';
-import NoPage from './components/NoPage';
+import NoPage from './components/Nopage';
 
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
       <Routes>
         <Routes path="/" elemet={<Register/>}/>
         <Routes path="/Login" elemet={<Login/>}/>
-        <Routes path="/NoPage" elemet={<NoPage/>}/>
+        <Routes path="/Nopage" elemet={<Nopage/>}/>
         <Routes path="/Homepage" elemet={<Homepage/>}/>
       </Routes>
       </BrowserRouter>
