40e8de8bef7929ffe0c02e8c2b11be2530ca289e
Jason <murangira99@gmail.com>
2024-08-01 19:35:24 +0200

    added imports from axios to use sate

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.min"
+import '../App.css'
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from "axios"
 
 const Register = () => {
 
