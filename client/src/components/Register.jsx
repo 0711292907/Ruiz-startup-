7a783cad6a660cbad53b5a0744bde6d499a8b85e
Jason <murangira99@gmail.com>
2024-08-01 19:37:44 +0200

    removed const use tste

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,8 +1,2 @@
-import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.bundle.min"
-import '../App.css'
-import { Link, useNavigate } from "react-router-dom"
-import { useState } from "react"
-import axios from "axios"
+
 
