0e86336b1ec93296539ce6c3d8fbe16dd8ef74c5
Jason <murangira99@gmail.com>
2024-09-03 17:45:17 +0200

    save

--- backend/connection.js
+++ backend/connection.js
@@ -2,6 +2,7 @@ const {createPool} = require('mysql2')
 
 const pool = createPool({
     host: process.env.HOST,
+    port: process.env.AVN_PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
